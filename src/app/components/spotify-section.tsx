f641cb68ab10872fab98984f6e25c0fe1b38e445
Heri Roque <heriroque.22@gmail.com>
2025-04-22 20:15:28 -0500

    Remove autoplay permission from Spotify iframe

--- src/app/components/spotify-section.tsx
+++ src/app/components/spotify-section.tsx
@@ -30,7 +30,7 @@ export default function SpotifySection() {
                   width="100%"
                   height="100%"
                   frameBorder="0"
-                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                  allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                   loading="lazy"
                 ></iframe>
               </div>
